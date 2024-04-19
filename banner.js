fffcc4f46b117974e7599ca3122590f9764081da
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2024-04-19 21:45:05 +0800

    Made the hello clickable

--- banner.js
+++ banner.js
@@ -1,6 +1,7 @@
 // Make the red banner to turn green when hover can be implemented using CSS but let me use JS vanilla through DOM here
 
 var bgRed = document.querySelector(".bg-red")
+var h1 = document.querySelector("h1 span")
 
 bgRed.addEventListener("mouseover", function(){
     bgRed.classList.add('bg-green')
@@ -8,4 +9,11 @@ bgRed.addEventListener("mouseover", function(){
 
 bgRed.addEventListener("mouseout", function() {
     bgRed.classList.remove("bg-green");
+});
+
+h1.addEventListener("click", function() {
+    if(h1.innerHTML == "Hello")
+        h1.innerHTML = "Jaydee"
+    else
+        h1.innerHTML = "Hello"
 });
